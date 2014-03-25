76896de0dda7aedf2624b0903e403f09074f6abf
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2014-03-25 17:03:43 +0000

    Ensure the container doesn't exit immediately if a tty isn't attached

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ asweb="setuser www-data"
 
 die () {
     msg=$1
-    echo "FATAL ERROR: " msg  > 2
+    echo "FATAL ERROR: " msg > 2
     exit
 }
 
@@ -114,3 +114,12 @@ for arg
 do
     $arg;
 done
+
+# Unless there is a terminal attached don't exit, otherwise docker
+# will also exit
+if ! tty --silent
+then
+    # Wait forever (see
+    # http://unix.stackexchange.com/questions/42901/how-to-do-nothing-forever-in-an-elegant-way).
+    tail -f /dev/null
+fi
