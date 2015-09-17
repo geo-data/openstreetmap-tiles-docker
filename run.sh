df6f989e521fad671cac768ec0d45480f74fcf12
SilentT <contact@silentt.fr>
2015-09-17 20:13:31 +0200

    Use spatial sql include postgis
    remove     # Add the 900913 Spatial Reference System
        $asweb psql -d $dbname -f /usr/local/share/osm2pgsql/900913.sql
    
    because already include in the spatial_ref_sys.sql
    
    http://wiki.openstreetmap.org/wiki/PostGIS/Installation#Ubuntu_14.04_LTS_2
    
    http://packages.ubuntu.com/fr/trusty-updates/all/postgresql-9.3-postgis-scripts/filelist

--- run.sh
+++ run.sh
@@ -50,14 +50,14 @@ createdb () {
 
     # Install the Postgis schema
     $asweb psql -d $dbname -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql
+    $asweb psql -d $dbname -f /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql
 
     $asweb psql -d $dbname -c 'CREATE EXTENSION HSTORE;'
 
     # Set the correct table ownership
     $asweb psql -d $dbname -c 'ALTER TABLE geometry_columns OWNER TO "www-data"; ALTER TABLE spatial_ref_sys OWNER TO "www-data";'
 
-    # Add the 900913 Spatial Reference System
-    $asweb psql -d $dbname -f /usr/local/share/osm2pgsql/900913.sql
+
 }
 
 import () {
